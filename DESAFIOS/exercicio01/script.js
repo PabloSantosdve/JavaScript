66dad8cc52c4aba4229ecca32643c96b0cc71f70
PabloSantosdve <148356304+PabloSantosdve@users.noreply.github.com>
2024-02-01 11:48:01 -0300

    Update script.js

--- DESAFIOS/exercicio01/script.js
+++ DESAFIOS/exercicio01/script.js
@@ -1,13 +1,19 @@
 function carregar() {
 var msg = window.document.getElementById('msg')
-var img = window.document.getElementById('imagem')
+var img = window.document.getElementById('foto')
 var data = new Date()
-var hora = data.getHours()
+//var hora = data.getHours()
+var hora = 15
 msg.innerHTML = `Agora são ${hora} horas.`
-if(hora >= 0 && 12){
-	//BOM DIA
-   img.src
+} if (hora >= 0 && hora < 12 ){
+	img.src = 'manha.png'
+	document.body.style.backgroundColor = '#e2cd9f'
+} else if (hora >= 12 && hora <= 18){
+	img.src = 'tarde.png'
+} else {
+	img.src = 'noite.png'
 }
-}
+
+
 
 
