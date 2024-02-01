378b70fd72c14e387a67108974a78084603a74bb
PabloSantosdve <148356304+PabloSantosdve@users.noreply.github.com>
2024-02-01 10:33:56 -0300

    .

---
+++ DESAFIOS/exercicio01/script.js
@@ -0,0 +1,13 @@
+function carregar() {
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('imagem')
+var data = new Date()
+var hora = data.getHours()
+msg.innerHTML = `Agora são ${hora} horas.`
+if(hora >= 0 && 12){
+	//BOM DIA
+   img.src
+}
+}
+
+
