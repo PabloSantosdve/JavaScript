a19f00e5e26ce5ca434abe8d24c878a14888a539
PabloSantosdve <148356304+PabloSantosdve@users.noreply.github.com>
2024-04-08 13:08:14 -0300

    fiz uma pagina que modifica o fundo

---
+++ javascript/mudando_o_fundo.js
@@ -0,0 +1,8 @@
+let element = document.getElementById("header")
+function modo_escuro(){
+
+	document.getElementById("fundo").style.backgroundColor="black";
+	document.getElementById("fundo").style.color="white";
+	document.getElementById("myImage").src="imagens/jordan_5.jpg";
+	element.innerHTML= "Testando o modo escuro!!!"
+}
